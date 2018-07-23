73bf5696be1ed9313efb8332260501f0668ca319
EdPalaciosV <40221943+EdPalaciosV@users.noreply.github.com>
2018-07-23 15:52:04 -0500

    commit

--- app/src/main/java/com/example/eduardopalacios/intentsimplicitas/MainActivity.java
+++ app/src/main/java/com/example/eduardopalacios/intentsimplicitas/MainActivity.java
@@ -1,64 +1,56 @@
 package com.example.eduardopalacios.intentsimplicitas;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-    Button web, call,maps;
+public class MainActivity extends AppCompatActivity {
+
+
     Intent intent;
+    @BindView(R.id.btnActionWeb)
+    Button btnActionWeb;
+    @BindView(R.id.btnActionCall)
+    Button btnActionCall;
+    @BindView(R.id.btnActionMaps)
+    Button btnActionMaps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        web = findViewById(R.id.btnActionWeb);
-        web.setOnClickListener(this);
-
-        call = findViewById(R.id.btnActionCall);
-        call.setOnClickListener(this);
-
-        maps=findViewById(R.id.btnActionMaps);
-        maps.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-
+    @OnClick({R.id.btnActionWeb, R.id.btnActionCall, R.id.btnActionMaps})
+    public void onViewClicked(View view) {
         switch (view.getId()) {
+
             case R.id.btnActionWeb:
-
                 intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://www.youtube.com"));
+                intent.setData(Uri.parse("https://www.youtube.com/?hl=es-419&gl=MX"));
                 startActivity(intent);
-
                 break;
             case R.id.btnActionCall:
-
                 intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:57582333"));
+                intent.setData(Uri.parse("tel:576753223"));
                 startActivity(intent);
-
                 break;
-
             case R.id.btnActionMaps:
                 intent = new Intent(Intent.ACTION_VIEW);
+                //Uri nos permite identificar el recurso
                 intent.setData(Uri.parse("geo:19.4978,-99.1269"));
                 startActivity(intent);
                 break;
-
-                default:
-                    Toast.makeText(getApplicationContext(),"valor no encontrado",Toast.LENGTH_SHORT).show();
-                    break;
         }
     }
 }
